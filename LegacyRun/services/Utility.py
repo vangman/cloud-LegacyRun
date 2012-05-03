b79f156d6341112d0b95e832dd1529878f6edc20
Vangelis Floros <efloros@grnet.gr>
2012-05-03 23:09:11 +0300

    removed classes and kept only list2dic method

--- LegacyRun/services/Utility.py
+++ LegacyRun/services/Utility.py
@@ -5,22 +5,14 @@ Utility funcions used by other modules
 
 @author: vangelis
 '''
-import time
 
-def list2dic(inlist):
+    
+def list2dic(self, inlist):
     outdic = {}
     for itemdic in inlist:
         for (key, value) in itemdic.iteritems():
             outdic[key]=value
     
     return outdic
-    
-def webStream(infile, refreshrate):
-    f = open(infile,"r")
-    eof = False
-    while not eof:
-        line = f.readline()
-        if line!='':
-            yield line
-        else:
-            time.sleep(refreshrate)
+        
+            
